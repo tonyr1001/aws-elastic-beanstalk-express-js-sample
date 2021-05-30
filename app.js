4e99191bdf53b1264486ec0466158c537e043ad2
tonyr1001 <83990019+tonyr1001@users.noreply.github.com>
2021-05-30 20:09:38 +1200

    this time

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-<<<<<<< Updated upstream
-app.get('/', (req, res) => res.send('Hello World!'));
-=======
+
 app.get('/', (req, res) => res.send('Hello World From Tony!'));
->>>>>>> Stashed changes
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
